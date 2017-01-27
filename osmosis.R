d012174d54c0cae658759896c9725cbea3b6bb1c
aecahill <aecsk8gr8@yahoo.com>
2017-01-27 17:07:52 -0500

    osmosis color change
    Fixed with better palette

--- osmosis.R
+++ osmosis.R
@@ -1,5 +1,6 @@
-Library(ggplot2)
+library(ggplot2)
 osmo<-read.table("C:/Users/acahill/Desktop/osmoregulation.txt",header=TRUE)
+MyPalette <-c("#8857BA","#A3E331", "#4EA087", "#3C6254","#DABB00", "#BD0707", "#7B2323",  "#A64C4C")
 
 ggplot(osmo,aes(Time,percent_change2))+
   geom_point(aes(color=Organism),size=4)+
@@ -13,6 +14,6 @@ ggplot(osmo,aes(Time,percent_change2))+
   theme(axis.text.y= element_text(size=16))+
   theme(axis.title.x=element_text(size=16))+
   theme(axis.title.y=element_text(size=16))+
-  scale_colour_manual(values=c("red3","gold1","grey40","black","blue","green4","green","olivedrab1"))+
+  scale_colour_manual(values=MyPalette)+
   ylim(0.95,1.15)+
   xlab("\nTime (minutes)")+ylab("Percent change \n")
